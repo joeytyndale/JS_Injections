db69bfa92457bdd8c10468dcc1926264cf19f953
Joseph Horning <jhorning@linkedin.biz>
2020-03-19 14:57:03 -0700

    Fixed bug

--- ins.js
+++ ins.js
@@ -25,7 +25,7 @@ $(document).ready(() => {
                     var profileURL = "https://customer.www.linkedin.com/support/account/urn:li:enterpriseAccount:" + accountId + "/profiles/" + profileId + "/licenses";
 
                     var profileButton = document.createElement("A");
-                    profileButton.innerHTML = `${companyName}(${accountId}) - Profile: ${profileId} Acct: ${accountI}`;
+                    profileButton.innerHTML = `${companyName}(${accountId}) - Profile: ${profileId}`;
                     profileButton.setAttribute("href",profileURL);
                     profileButton.setAttribute("target","_blank");
 
